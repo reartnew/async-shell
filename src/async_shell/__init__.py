b52c121699d9338f7cc5629ffc19ab343c06250a
reartnew <artnew@list.ru>
2023-01-23 14:43:43 +0100

    Shorter everything

--- src/async_shell/__init__.py
+++ src/async_shell/__init__.py
@@ -12,21 +12,21 @@ from subprocess import PIPE
 
 from .version import __version__
 
-AST = t.TypeVar("AST", bound="AsyncSubprocessShell")
+ST = t.TypeVar("ST", bound="Shell")
 
 __all__ = [
-    "AsyncSubprocessShellResult",
-    "AsyncSubprocessShellError",
-    "AsyncSubprocessShell",
+    "ShellResult",
+    "ShellError",
+    "Shell",
 ]
 
 
-class AsyncSubprocessShellError(Exception):
+class ShellError(Exception):
     """Subprocess non-zero exit code failure on .validate() call"""
 
     _prefix: str = "    "
 
-    def __init__(self, result: AsyncSubprocessShellResult) -> None:
+    def __init__(self, result: ShellResult) -> None:
         msg: str = f"Subprocess failed with code: {result.code}\n"
         if result.stdout:
             msg += f"{self._prefix}PROCESS STDOUT:\n{textwrap.indent(result.stdout, self._prefix * 2)}\n"
@@ -36,7 +36,7 @@ class AsyncSubprocessShellError(Exception):
 
 
 @dataclass
-class AsyncSubprocessShellResult:
+class ShellResult:
     """Subprocess result container"""
 
     stdout: str
@@ -47,14 +47,14 @@ class AsyncSubprocessShellResult:
     def __bool__(self) -> bool:
         return bool(self.code)
 
-    def validate(self) -> AsyncSubprocessShellResult:
+    def validate(self) -> ShellResult:
         """Raise SubprocessError on failure"""
         if self.code:
-            raise AsyncSubprocessShellError(self)
+            raise ShellError(self)
         return self
 
 
-class AsyncSubprocessShell:
+class Shell(t.Awaitable[ShellResult]):
     """Asyncio subprocess wrapper"""
 
     def __init__(
@@ -66,6 +66,12 @@ class AsyncSubprocessShell:
         self._proc: t.Optional[Process] = None
         self._encoding: str = encoding or ("cp866" if os.name == "nt" else "utf-8")
         self._start_time: t.Optional[float] = None
+        self._post_validate: bool = False
+
+    def validate(self) -> Shell:
+        """Mark shell subprocess as requiring validation after evaluation"""
+        self._post_validate = True
+        return self
 
     async def _get_proc(self) -> Process:
         if self._proc is None:
@@ -92,23 +98,39 @@ class AsyncSubprocessShell:
         async for chunk in stderr:  # type: bytes
             yield chunk.decode(self._encoding)
 
-    async def _await(self):
+    async def _await(self) -> ShellResult:
         proc: Process = await self._get_proc()  # type: ignore
         stdout_bytes, stderr_bytes = await proc.communicate()
-        return AsyncSubprocessShellResult(
+        result = ShellResult(
             stdout=stdout_bytes.decode(self._encoding),
             stderr=stderr_bytes.decode(self._encoding),
-            code=proc.returncode,
-            time=time.perf_counter() - self._start_time,
+            code=proc.returncode,  # type: ignore
+            time=time.perf_counter() - self._start_time,  # type: ignore
         )
+        if self._post_validate:
+            result.validate()
+        return result
 
     def __await__(self):
         # pylint: disable=no-member
         return self._await().__await__()
 
-    async def __aenter__(self: AST) -> AST:
+    def __or__(self, other: Shell) -> Shell:
+        if self._encoding != other._encoding:
+            raise ValueError(f"Encoding mismatch: {self._encoding!r} != {other._encoding!r}")
+        if self._start_time is not None:
+            raise RuntimeError(f"Process {self} has already been started")
+        if other._start_time is not None:
+            raise RuntimeError(f"Process {other} has already been started")
+        result = Shell(command=f"{self._command} | {other._command}", encoding=self._encoding)
+        if self._post_validate or other._post_validate:
+            result.validate()
+        return result
+
+    async def __aenter__(self: ST) -> ST:
         await self._get_proc()
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb) -> bool:
+        # TODO: graceful finish
         return exc_type is None
