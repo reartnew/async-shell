451760aa5731f39012b099859010a6b53b6d4b80
Artem Novikov <reartnew@yandex-team.ru>
2023-02-06 11:33:18 +0100

    Fewer deps, easier tests

--- tests/test_execution.py
+++ tests/test_execution.py
@@ -2,60 +2,29 @@
 from __future__ import annotations
 
 import os
-from typing import TypedDict
+import typing as t
 
 import pytest
-from pytest_data_suites import DataSuite
 
-from async_shell import Shell
-from async_shell.constants import IS_MACOS, IS_WIN32
+from async_shell import Shell, ShellResult
 
-
-class ShellCase(TypedDict):
-    """Execution case template"""
-
-    executable: str
-    command: str
+TEST_COMMAND: str = "echo a && echo b"
 
 
-class WindowsShellDataSuite(DataSuite):
-    """Windows cases"""
-
-    win_cmd = ShellCase(executable="cmd", command="dir")
-    win_powershell = ShellCase(executable="powershell", command="dir")
-
-
-class MacOSShellDataSuite(DataSuite):
-    """MacOS cases"""
-
-    # Use script for tty
-    mac_os_sh = ShellCase(executable="script -q /dev/null sh --rcfile <(echo PS1='$')", command="ls -la")
-    mac_os_bash = ShellCase(executable="script -q /dev/null bash --rcfile <(echo PS1='$')", command="ls -la")
-
-
-class LinuxShellDataSuite(DataSuite):
-    """Linux cases"""
-
-    # Use script for tty
-    linux_sh = ShellCase(executable="script -qc sh /dev/null", command="pwd")
-    linux_bash = ShellCase(executable="script -qc bash /dev/null", command="pwd")
-
-
-ShellDataSuite = WindowsShellDataSuite if IS_WIN32 else MacOSShellDataSuite if IS_MACOS else LinuxShellDataSuite
-
-
-@ShellDataSuite.parametrize
 @pytest.mark.asyncio
-async def test_async_subprocess_call(command: str) -> None:
+async def test_async_subprocess_call() -> None:
     """Test simple call"""
-    os_result = await Shell(command)
+    os_result = await Shell(TEST_COMMAND)
     assert not os_result.code
 
 
-@ShellDataSuite.parametrize
 @pytest.mark.asyncio
 @pytest.mark.parametrize("strip_linesep", [True, False], ids=["with-strip", "without-strip"])
-async def test_stream_reader(command: str, strip_linesep: bool) -> None:
+async def test_stream_reader(strip_linesep: bool) -> None:
     """Validate stdout reader"""
-    async for line in Shell(command).read_stdout(strip_linesep=strip_linesep):
+    lines: t.List[str] = []
+    async for line in Shell(TEST_COMMAND).read_stdout(strip_linesep=strip_linesep):
         assert line.endswith(os.linesep) != strip_linesep
+        lines.append(line.rstrip())
+    clean_result: ShellResult = await Shell(TEST_COMMAND).validate()
+    assert lines == clean_result.stdout.splitlines()
